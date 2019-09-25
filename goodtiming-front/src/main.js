1aceda3fef0567b2c2f067e027e34f5815ff2625
Paul Busby <paul.busby@gmail.com>
2019-09-25 14:57:14 +0100

    Created a dashboard view, connected vue-router and created a route for the dashboard, connected Axios and configured an API call for stopwatches

--- goodtiming-front/src/main.js
+++ goodtiming-front/src/main.js
@@ -1,19 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import store from './store'
 import VueAxios from 'vue-axios'
-import { plainAxiosInstance } from './backend/axios'
 
 Vue.config.productionTip = false
-
-Vue.use(VueAxios, {
-  plain: plainAxiosInstance
-})
 
 new Vue({
   router,
   store,
-  plainAxiosInstance,
   render: h => h(App)
 }).$mount('#app')
---
+++ goodtiming-front/src/views/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Welcome to the dashboard!</h1>
+    <div class v-for="(stopwatch, index) in wholeResponse" :key="index">
+      <h4>{{stopwatch.name}}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      wholeResponse: []
+    };
+  },
+  mounted() {
+    var self = this;
+    axios
+      .get("http://localhost:3000/api/v1/stopwatches")
+      .then(response => {
+        self.wholeResponse = response.data.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
