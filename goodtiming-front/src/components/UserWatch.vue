24dfd877af55cc191036a4bb55314f30c211864a
Paul Busby <paul.busby@gmail.com>
2019-11-03 10:55:57 +0000

    Added svg loading animations

--- goodtiming-front/src/components/UserWatch.vue
+++ goodtiming-front/src/components/UserWatch.vue
@@ -2,10 +2,13 @@
   <div>
     <h1>{{singleWatch.name}}</h1>
     <h2 ref="timer">00 : 00 . 000</h2>
-    <p>My id is: {{singleWatch.id}}</p>
-    <p>My description is: {{singleWatch.description}}</p>
-    <p>My cumulative time is: {{cumulativeTime}}</p>
-    <p>My elapsed time is: {{elapsedTime}}</p>
+    <p class="mt-3">MOTIVATION...</p>
+    <p>{{singleWatch.description}}<p>
+    <p>ELAPSED TO DATE...</p>
+    <h5>{{cumulativeTime}}</h5>
+    <p>ELAPSED CURRENTLY...</p>
+    <h5 class="mb-3">{{elapsedTime}}</h5>
+
 
     <button class v-if="!jsWatch.isOn" @click="watchHandler()">Start</button>
     <!-- <button class @click="getStartTime">Start</button> -->
@@ -31,7 +34,11 @@ export default {
       endTime: 0,
       elapsedTime: 0,
       cumeTime: 0,
+      years: 0,
+      days: 0,
+      minutes: 0,
       isOn: false,
+      apiLoading: true,
     };
   },
   computed: {
@@ -41,8 +48,33 @@ export default {
     computedjsWatch: function(){
       return this.jsWatch(this.$refs.timer)
     },
+    // y(secs){
+    //    if(secs % 31536000 !== secs){
+    //       this.years = 0
+    //       return secs
+    //    } else {
+    //      return secs * (secs % 31536000)
+    //    }
+    // },
+    // m(secs){
+    //   if(secs % d !== secs){
+    //     this.months = 0
+    //     return secs
+    //   }else {
+    //     return secs * (secs % )
+    //   }
+    // },
+    // d(secs){
+    //   if(secs % d !== secs){
+    //     this.days = 0
+    //     return secs
+    //   }
+    // },
+    
     cumulativeTime: function(){
       return this.cumeTime = this.cumeTime + Math.round(this.elapsedTime)
+      // d(m(y(this.cumeTime)))
+      // return `Days: ${Math.round(day)} Minutes: ${Math.round(minutes)} Seconds: ${Math.round(seconds)}`
     }
   },
   methods: {
@@ -70,6 +102,10 @@ export default {
         this.getStartTime();
       }
     },
+    apiUpdate(){
+      this.apiLoading = false;
+      this.$emit('event_child', false)
+    },
     reset(){
       this.startTime = 0;
       this.endTime = 0;
@@ -81,7 +117,7 @@ export default {
         e.preventDefault();
         let currentObj = this;
         let updatedTime = this.cumeTime
-        this.axios.patch(`https://goodtiming.herokuapp.com/api/v1/stopwatches/${this.singleWatch.id}`, {
+        this.axios.patch(`http://localhost:3000/api/v1/stopwatches/${this.singleWatch.id}`, {
             total_time: updatedTime
         })
         .then(function (response) {
@@ -96,10 +132,12 @@ export default {
     this.jsWatch = new JsWatch(this.$refs.timer)
     var self = this;
     axios
-      .get(`https://goodtiming.herokuapp.com/api/v1/stopwatches/${self.id}`)
+      .get(`http://localhost:3000/api/v1/stopwatches/${self.id}`)
       .then(response => {
         self.singleWatch = response.data.data;
         self.cumeTime = self.singleWatch.total_time
+        self.apiUpdate()
+
       })
       .catch(error => {
         console.log(error);
---
+++ goodtiming-front/src/views/Stopwatch.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <UserWatch :id="this.id" v-show="!apiLoading && !tOut" v-on:event_child="apiUpdate" />
+    <StopwatchPlaceholder v-show="apiLoading || tOut" />
+  </div>
+</template>
+<script>
+import UserWatch from '@/components/UserWatch.vue'
+import StopwatchPlaceholder from '@/components/StopwatchPlaceholder.vue'
+
+
+export default {
+  name: "Stopwatch",
+  components: {
+      UserWatch,
+      StopwatchPlaceholder
+  },
+  props: ["id"],
+
+  data() {
+    return {
+        apiLoading: true,
+        tOut: true
+    };
+  },
+  computed: {
+    
+  },
+  methods: {
+    apiUpdate: function(){
+    this.apiLoading = false;
+    },
+    timeout(){
+      setTimeout(() => this.tOut = false, 1000)
+    },
+  },
+  mounted() {
+    this.timeout()
+
+  }
+};
+</script>
